bb7efc8e4d0a9dbf95f4435b979d1e6955829ec0
Khululekani Mkhonza <khululekanim@e2save.com>
2018-04-21 23:27:32 +0100

    initialise the project and add our dependancies

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('exoress');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(morgan('dev'));
+
+
