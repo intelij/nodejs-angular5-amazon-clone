902ef263a2fc0ee7657b681b2965c2b4b5c2d56a
Khululekani Mkhonza <khululekanim@e2save.com>
2018-04-21 23:38:44 +0100

    lets start our server and tell it which port to listen to

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const express = require('exoress');
+const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -9,4 +9,8 @@ app.use(bodyParser.json);
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(morgan('dev'));
 
+
+app.listen(3030, err => {
+   console.log('We are here listening to port 3030..')
+});
 
